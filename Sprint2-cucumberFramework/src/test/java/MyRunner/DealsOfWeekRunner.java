528da78b536cf48c794c96a916d421aa89d002ad
Ajinky <Ajinky@LAPTOP-T66E8UCB.mshome.net>
2021-10-13 13:01:18 +0530

    first commit

--- Sprint2-cucumberFramework/src/test/java/MyRunner/DealsOfWeekRunner.java
+++ Sprint2-cucumberFramework/src/test/java/MyRunner/DealsOfWeekRunner.java
@@ -4,12 +4,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "./src/test/java/Features/LoginBB.feature",
+		features = "./src/test/java/Features/DealsOfWeek.feature",
 		glue = "StepDefinition",
 		plugin = {"pretty","html:Reports/HTMLReport.html","json:target/Jsonreport.json",
 				"junit:target/JUnitreport.junit"}
 		)
 
-public class LoginRunner extends AbstractTestNGCucumberTests{
+public class DealsOfWeekRunner extends AbstractTestNGCucumberTests{
 
 }
